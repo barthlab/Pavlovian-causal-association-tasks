7143c35dfc17a6f0ed01fec23f83b40df44c43a3
maxyc <maxycc@outlook.com>
2025-06-30 19:05:53 -0400

    fix buzzer bugs

--- tools/Buzzer.py
+++ tools/Buzzer.py
@@ -6,12 +6,16 @@ PWM tone generator
 
 import RPi.GPIO as GPIO
 import Config as Config
+import time
 
 
 class Buzzer:
     def __init__(self, buzzer_pin: int, frequency: int):
         GPIO.setup(buzzer_pin, GPIO.OUT)
         self.pwm = GPIO.PWM(buzzer_pin, frequency)
+        self.on()
+        time.sleep(0.1)
+        self.stop()
 
     def on(self):
         # Start PWM: pwm.start(duty_cycle)
