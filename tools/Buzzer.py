31fa435efcb6f7b362297dad211b6a70a7aaa1de
maxyc <maxycc@outlook.com>
2025-07-17 11:39:47 -0400

    fix buzzer issue

--- tools/Buzzer.py
+++ tools/Buzzer.py
@@ -7,12 +7,19 @@ PWM tone generator
 import RPi.GPIO as GPIO
 import Config as Config
 import time
+from utils.PinManager import Pin
 
 
 class Buzzer:
     def __init__(self, buzzer_pin: int, frequency: int):
         GPIO.setup(buzzer_pin, GPIO.OUT)
-        self.pwm = GPIO.PWM(buzzer_pin, frequency)
+        if buzzer_pin != 33:
+            self.buzzer = Pin(buzzer_pin, GPIO.OUT)
+            self.buzzer.output(GPIO.LOW)
+            self.pwm_flag = False
+        else:
+            self.buzzer = GPIO.PWM(buzzer_pin, frequency)
+            self.pwm_flag = True
         self.on()
         time.sleep(0.1)
         self.stop()
@@ -20,10 +27,16 @@ class Buzzer:
     def on(self):
         # Start PWM: pwm.start(duty_cycle)
         # 50% duty cycle is a good choice for a simple square wave tone.
-        self.pwm.start(50)
+        if self.pwm_flag:
+            self.buzzer.start(50)
+        else:
+            self.buzzer.output(GPIO.HIGH)
 
     def stop(self):
-        self.pwm.stop()
+        if self.pwm_flag:
+            self.buzzer.stop()
+        else:
+            self.buzzer.output(GPIO.LOW)
 
 
 def GetBuzzer(*args):
